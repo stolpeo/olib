ad80cd57b976c0efefc2430f0ec9352f7b35f8c3
Oliver Stolpe <oliver.stolpe@bihealth.de>
2017-01-03 14:38:23 +0100

    added memory footprint

--- olib/olib.py
+++ olib/olib.py
@@ -1,13 +1,18 @@
 import time
+import resource
 
 def debug(active=True):
     def decorate(func):
         def clocked(*args):
-            t0 = time.perf_counter()
+            mem_init = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+            t0 = time.time()
             res = func(*args)
-            ela = time.perf_counter() - t0
+            ela = time.time() - t0
+            mem_final = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+            mem_diff = mem_final - mem_init
             args = ','.join(map(repr, args))
-            print("[{0:.8f}s] {1}({2}) -> {3}".format(ela, func.__name__, args, repr(res)))
+            print("[{0:.8f} s | {4:,} B | {5:,} B] {1}({2}) -> {3}".format(ela, func.__name__, args, repr(res), mem_diff, mem_final))
+
             return res
 
         if active:
